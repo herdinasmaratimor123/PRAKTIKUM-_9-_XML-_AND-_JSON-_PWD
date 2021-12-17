acc6ecf9a91028f34f8fce18f01eb717afdc0313
herdinasmaratimor123 <62454502+herdinasmaratimor123@users.noreply.github.com>
2021-12-17 17:56:23 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $hostname  = "localhost";
+    $username  = "root";
+    $password  = "";
+    $dbname  = "kademik";
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);    
+?>
